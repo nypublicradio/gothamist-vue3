8234bdecaa4363845adb92f2dfd224aa0f45945a
Kim LaRocca <kim@kimlarocca.com>
2022-06-30 15:44:40 -0400

    load in goth theme and cards (#11)
    * load in goth theme and cards
    
    * formatting
    
    * updated section slug to use cards
    
    * updated tag slug to use cards

--- pages/[sectionSlug]/index.vue
+++ pages/[sectionSlug]/index.vue
@@ -1,30 +1,57 @@
 <script setup lang="ts">
-  import { fuzzyDateTime } from '~~/utilities/date';
+import { fuzzyDateTime } from '~~/utilities/date'
+import VCard from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VCard.vue'
+import VByline from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VByline.vue'
 
-  const route = useRoute()
-  const { title:sectionTitle, id:sectionId } = await findPage(route.params.sectionSlug as string) 
-    .then(({data}) => normalizeFindPageResponse(data))
-  const articles  =  await findArticlePages({ descendant_of: sectionId })
-    .then(({data}) => normalizeFindArticlePagesResponse(data))
+const config = useRuntimeConfig()
+const route = useRoute()
 
-  onMounted(() => {
-    const { $analytics } = useNuxtApp()
-    $analytics.sendPageView({ page_type: 'section_page' })
-  })
-</script> 
+const { title: sectionTitle, id: sectionId } = await findPage(
+  route.params.sectionSlug as string
+).then(({ data }) => normalizeFindPageResponse(data))
+
+const articles = await findArticlePages({ descendant_of: sectionId }).then(
+  ({ data }) => normalizeFindArticlePagesResponse(data)
+)
+
+onMounted(() => {
+  const { $analytics } = useNuxtApp()
+  $analytics.sendPageView({ page_type: 'section_page' })
+})
+</script>
 
 <template>
   <div>
     <h1>{{ sectionTitle }}</h1>
     <div v-for="article in articles" :key="article.uuid">
-      <NuxtLink :to="`/${article.section.slug}`">{{article.section.name}}</NuxtLink><br>
-      <NuxtLink :to="article.link"><b>{{ article.title }}</b></NuxtLink><br>
-      <span>{{ article.description }}</span><br>
-      <span>{{ fuzzyDateTime(article.publicationDate) }}</span><br>
-      <span v-if="article.updatedDate">Updated: {{ fuzzyDateTime(article.updatedDate) }}</span><br>
-      <ul>
-        <li v-for="tag in article.tags" :key="tag.slug"><NuxtLink :to='`/tags/${tag.slug}`'>{{ tag.name }}</NuxtLink></li>
-      </ul>
+      <v-card
+        class="mod-horizontal mb-5"
+        :image="`${config.IMAGE_BASE_URL}${article.image.id}/fill-318x214|format-jpeg|jpegquality-70`"
+        :title="article.title"
+        :titleLink="article.link"
+        :ratio="[3, 2]"
+        :width="318"
+        :height="214"
+        :maxWidth="article.image.width"
+        :maxHeight="article.image.height"
+        :tags="[
+          {
+            name: article.section.name,
+            slug: article.section.slug,
+          },
+        ]"
+      >
+        <p>
+          {{ article.description }}
+        </p>
+        <div class="article-metadata">
+          <span>
+            <v-byline :authors="article.authors" />
+          </span>
+          <span>comments go here</span>
+        </div>
+      </v-card>
+      <hr class="mb-5" />
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,55 @@
 <script setup lang="ts">
-  import { fuzzyDateTime } from '../utilities/date'
+import { fuzzyDateTime } from '../utilities/date'
+import VCard from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VCard.vue'
+import VByline from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VByline.vue'
 
-  const articles = await findArticlePages("")
-    .then(({data}) => normalizeFindArticlePagesResponse(data))
+const config = useRuntimeConfig()
 
-  onMounted(() => {
-    const { $analytics } = useNuxtApp()
-    $analytics.sendPageView({ page_type: 'home_page' })
-  })
+const articles = await findArticlePages('').then(({ data }) =>
+  normalizeFindArticlePagesResponse(data)
+)
+
+onMounted(() => {
+  const { $analytics } = useNuxtApp()
+  $analytics.sendPageView({ page_type: 'home_page' })
+})
 </script>
 
 <template>
   <div>
-    <h2>Featured Articles</h2>
-    <div v-for="article in articles" :key="article.uuid">
-      <NuxtLink :to="`/${article.section.slug}`">{{ article.section.name }}</NuxtLink><br>
-      <NuxtLink :to="article.link"><b>{{ article.title }}</b></NuxtLink><br>
-      <span>{{ article.description }}</span><br>
-      <span>{{ fuzzyDateTime(article.publicationDate) }}</span><br>
-      <span v-if="article.updatedDate">Updated: {{ fuzzyDateTime(article.updatedDate) }}</span><br>
-    </div>  
+    <template v-if="articles">
+      <div v-for="article in articles" :key="article.uuid">
+        <v-card
+          class="mod-horizontal mb-5"
+          :image="`${config.IMAGE_BASE_URL}${article.image.id}/fill-318x214|format-jpeg|jpegquality-70`"
+          :title="article.title"
+          :titleLink="article.link"
+          :ratio="[3, 2]"
+          :width="318"
+          :height="214"
+          :maxWidth="article.image.width"
+          :maxHeight="article.image.height"
+          :tags="[
+            {
+              name: article.section.name,
+              slug: article.section.slug,
+            },
+          ]"
+        >
+          <p>
+            {{ article.description }}
+          </p>
+          <div class="article-metadata">
+            <span>
+              <v-byline :authors="article.authors" />
+            </span>
+            <span>comments go here</span>
+          </div>
+        </v-card>
+        <hr class="mb-5" />
+      </div>
+    </template>
   </div>
 </template>
+
+<style lang="scss"></style>
