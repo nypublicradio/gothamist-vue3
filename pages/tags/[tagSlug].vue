182094a9e2dad07cb9ac5d579f340d790fdbdca7
Kim LaRocca <kim@kimlarocca.com>
2022-06-30 15:18:44 -0400

    updated tag slug to use cards

--- pages/tags/[tagSlug].vue
+++ pages/tags/[tagSlug].vue
@@ -1,45 +1,71 @@
 <script setup lang="ts">
-  import { TagPage } from '../../composables/types/Page'
-  import { fuzzyDateTime } from '../../utilities/date'
+import { TagPage } from '../../composables/types/Page'
+import { fuzzyDateTime } from '../../utilities/date'
+import VCard from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VCard.vue'
+import VByline from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VByline.vue'
 
-  const { $analytics, $htlbid } = useNuxtApp()
-  const route = useRoute()
-  const tagSlug = route.params.tagSlug
-  const curatedTagPage = await findPage(`tags/${tagSlug}`)
-    .then(({data}) => data?.value && normalizeFindPageResponse(data) as TagPage)
+const { $analytics, $htlbid } = useNuxtApp()
+const route = useRoute()
+const config = useRuntimeConfig()
+const tagSlug = route.params.tagSlug
+const curatedTagPage = await findPage(`tags/${tagSlug}`).then(
+  ({ data }) => data?.value && (normalizeFindPageResponse(data) as TagPage)
+)
 
-  const articles = await findArticlePages({ tag_slug: tagSlug, limit:12, offset:0 })
-    .then(({data}) => normalizeFindArticlePagesResponse(data))
+const articles = await findArticlePages({
+  tag_slug: tagSlug,
+  limit: 12,
+  offset: 0,
+}).then(({ data }) => normalizeFindArticlePagesResponse(data))
 
-  const tagName = articles[0]?.tags.find(tag => tag.slug === tagSlug)?.name
-    || tagSlug
+const tagName =
+  articles[0]?.tags.find((tag) => tag.slug === tagSlug)?.name || tagSlug
 
-  onMounted(() => {
-    $analytics.sendPageView({ page_type: 'tag_page' })
-    $htlbid.setTargeting({ Template: 'Tag' })
-  })
+onMounted(() => {
+  $analytics.sendPageView({ page_type: 'tag_page' })
+  $htlbid.setTargeting({ Template: 'Tag' })
+})
 
-  onUnmounted(() => {
-    $htlbid.clearTargeting({ Template: 'Tag' })
-  })
-</script> 
+onUnmounted(() => {
+  $htlbid.clearTargeting({ Template: 'Tag' })
+})
+</script>
 
 <template>
   <div>
-    <h1>{{ tagName  }}</h1>
+    <h1>{{ tagName }}</h1>
     <div v-for="article in articles" :key="article.uuid">
       <div v-if="curatedTagPage?.topPageZone">
-        <pre>{{curatedTagPage.topPageZone}}</pre>
+        <pre>{{ curatedTagPage.topPageZone }}</pre>
       </div>
-      <NuxtLink :to="`/${article.section.slug}`">{{article.section.name}}</NuxtLink><br>
-      <NuxtLink :to="article.link"><b>{{ article.title }}</b></NuxtLink><br>
-      <span>{{ article.description }}</span><br>
-      <span>{{ fuzzyDateTime(article.publicationDate) }}</span><br>
-      <span v-if="article.updatedDate">Updated: {{ fuzzyDateTime(article.updatedDate) }}</span><br>
-      <ul>
-        <li v-for="tag in article.tags" :key="tag.slug"><NuxtLink :to='`/tags/${tag.slug}`'>{{ tag.name }}</NuxtLink></li>
-      </ul>
-
+      <v-card
+        class="mod-horizontal mb-5"
+        :image="`${config.IMAGE_BASE_URL}${article.image.id}/fill-318x214|format-jpeg|jpegquality-70`"
+        :title="article.title"
+        :titleLink="article.link"
+        :ratio="[3, 2]"
+        :width="318"
+        :height="214"
+        :maxWidth="article.image.width"
+        :maxHeight="article.image.height"
+        :tags="[
+          {
+            name: article.section.name,
+            slug: article.section.slug,
+          },
+        ]"
+      >
+        <p>
+          {{ article.description }}
+        </p>
+        <div class="article-metadata">
+          <span>
+            <v-byline :authors="article.authors" />
+          </span>
+          <span>comments go here</span>
+        </div>
+      </v-card>
+      <hr class="mb-5" />
     </div>
   </div>
 </template>
