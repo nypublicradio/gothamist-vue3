b0ec8831c5d15d6d82d7ac7766f5d7467905d5d0
Kim LaRocca <kim@kimlarocca.com>
2022-07-11 14:05:03 -0400

    article page / streamfield styles + updated DS

--- components/PullQuote.vue
+++ components/PullQuote.vue
@@ -1,10 +1,38 @@
-<!-- Placeholder Component-->
 <script setup lang="ts">
 defineProps<{
   quote: string
   author?: string
 }>()
 </script>
+
 <template>
-    <blockquote>{{ quote }}</blockquote><span v-if="author">{{ author }}</span>
+  <blockquote class="streamfield-pull-quote pt-4 pb-3">
+    <h3>
+      {{ quote }}
+    </h3>
+  </blockquote>
+  <span class="streamfield-pull-quote-author pb-4 mb-5" v-if="author">
+    {{ author }}
+  </span>
 </template>
+
+<style lang="scss">
+.streamfield-pull-quote {
+  border-top: solid 1px var(--black);
+  position: relative;
+  &::before {
+    content: url("data:image/svg+xml,%0A%3Csvg width='11' height='9' viewBox='0 0 11 9' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M3.35429 4.16047C3.84883 4.26797 4.22511 4.51524 4.48313 4.90226C4.76266 5.28928 4.90242 5.7623 4.90242 6.32133C4.90242 7.05237 4.67665 7.64365 4.22511 8.09517C3.79507 8.5467 3.21452 8.77246 2.48346 8.77246C1.7524 8.77246 1.15035 8.5467 0.677308 8.09517C0.225769 7.62215 0 7.04162 0 6.35358C0 5.68705 0.215018 4.97751 0.645055 4.22497L3.09626 0H4.90242L3.35429 4.16047ZM5.99901 6.35358C5.99901 5.68705 6.21403 4.97751 6.64407 4.22497L9.09527 0H10.9014L9.3533 4.16047C9.84784 4.26797 10.2241 4.51524 10.4821 4.90226C10.7617 5.28928 10.9014 5.7623 10.9014 6.32133C10.9014 7.05237 10.6757 7.64365 10.2241 8.09517C9.79408 8.5467 9.21353 8.77246 8.48247 8.77246C7.75141 8.77246 7.14936 8.5467 6.67632 8.09517C6.22478 7.62215 5.99901 7.04162 5.99901 6.35358Z' fill='%23111111'/%3E%3C/svg%3E%0A");
+    display: block;
+    padding: 9px 14px 11px;
+    border: solid 1px var(--text-color);
+    border-radius: 50%;
+    float: left;
+    margin-right: 10px;
+  }
+}
+.streamfield-pull-quote-author {
+  display: block;
+  border-bottom: solid 1px var(--gray-100);
+  font-size: var(--font-size-5);
+}
+</style>
--- pages/[sectionSlug]/[articleSlug].vue
+++ pages/[sectionSlug]/[articleSlug].vue
@@ -1,59 +1,81 @@
 <script setup lang="ts">
-  import { ArticlePage } from '../../composables/types/Page'
-  import { fuzzyDateTime } from '../../utilities/date'
+import VImageWithCaption from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VImageWithCaption.vue'
+import VTag from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VTag.vue'
+import { ArticlePage } from '../../composables/types/Page'
+import { fuzzyDateTime } from '../../utilities/date'
 
-  const route = useRoute()
-  const { $analytics, $htlbid } = useNuxtApp()
-  const article = await findPage(`${route.params.sectionSlug}/${route.params.articleSlug}`)
-    .then(({data}) => normalizeFindPageResponse(data)) as ArticlePage
+const route = useRoute()
+const { $analytics, $htlbid } = useNuxtApp()
+const article = (await findPage(
+  `${route.params.sectionSlug}/${route.params.articleSlug}`
+).then(({ data }) => normalizeFindPageResponse(data))) as ArticlePage
 
-  const trackingData = useArticlePageTrackingData(article)
-  const adTargetingData = useArticlePageAdTargetingData(article)
+const trackingData = useArticlePageTrackingData(article)
+const adTargetingData = useArticlePageAdTargetingData(article)
+const sensitiveContent = useSensitiveContent()
+const headMetadata = useArticlePageHeadMetadata(article)
+
+useHead(headMetadata)
+
+onMounted(() => {
+  $analytics.sendPageView(trackingData)
+  $htlbid.setTargeting(adTargetingData)
+  sensitiveContent.value = article.sensitiveContent
+})
+
+onUnmounted(() => {
+  $htlbid.clearTargeting(adTargetingData)
+  sensitiveContent.value = false
+})
+
+function handleArticleMounted(el) {
+  const landmarks = useStreamfieldLandmarks(el.value)
+  const adTarget = landmarks[Math.min(landmarks.length - 1, 5)].node
+  useInsertAd(adTarget)
+}
+
+function useInsertAd(targetElement) {
   const sensitiveContent = useSensitiveContent()
-  const headMetadata = useArticlePageHeadMetadata(article)
-
-  useHead(headMetadata)
-
-  onMounted(() => {
-    $analytics.sendPageView(trackingData)
-    $htlbid.setTargeting(adTargetingData)
-    sensitiveContent.value = article.sensitiveContent
-  })
-
-  onUnmounted(() => {
-    $htlbid.clearTargeting(adTargetingData)
-    sensitiveContent.value = false
-  })
-
-  function handleArticleMounted(el) {
-    const landmarks = useStreamfieldLandmarks(el.value)
-    const adTarget = landmarks[Math.min(landmarks.length - 1, 5)].node
-    useInsertAd(adTarget)
+  if (article && !sensitiveContent.value) {
+    const adDiv = document.createElement('DIV')
+    adDiv.classList.add(
+      'htlad-interior_midpage_1',
+      'ad-div',
+      'mod-break-margins',
+      'mod-ad-disclosure',
+      'mb-5'
+    )
+    useInsertAfterElement(adDiv, targetElement)
   }
-
-  function useInsertAd(targetElement) {
-    const sensitiveContent = useSensitiveContent()
-       if (article && !sensitiveContent.value) {
-        const adDiv = document.createElement('DIV')
-        adDiv.classList.add('htlad-interior_midpage_1',
-          'ad-div',
-          'mod-break-margins',
-          'mod-ad-disclosure')
-        useInsertAfterElement(adDiv, targetElement)
-      }
-  }
+}
 </script>
 
 <template>
-  <div v-if="article">
-    <h2>{{ article.title }}</h2>
-    <NuxtLink :to="`/${article.section.slug}`">{{article.section.name}}</NuxtLink><br>
-    <span>{{ article.description }}</span><br>
-    <span>{{ fuzzyDateTime(article.publicationDate) }}</span><br>
-    <span v-if="article.updatedDate">Updated: {{ fuzzyDateTime(article.updatedDate) }}</span><br>
-    <VStreamfield
-      :streamfieldBlocks="article.body"
-      @allBlocksMounted="handleArticleMounted"
-    />
+  <div>
+    <section>
+      <div class="content">
+        <div v-if="article">
+          <div class="text-left md:text-center mb-3">
+            <v-tag :name="article.section.name" :slug="article.section.slug" />
+            <h1 class="mt-4 mb-6">{{ article.title }}</h1>
+            <v-image-with-caption
+              :image="useImageUrl(article.listingImage)"
+              :alt-text="article.image.alt"
+              :maxWidth="article.image.width"
+              :maxHeight="article.image.height"
+              :description="article.image.caption"
+              :credit="`Photo by ${article.image.credit}`"
+              :credit-url="article.image.creditLink"
+              :ratio="[3, 2]"
+            />
+          </div>
+          <hr class="mb-7" />
+          <v-streamfield
+            :streamfield-blocks="article.body"
+            @all-blocks-mounted="handleArticleMounted"
+          />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
