d7213eb6b66e7158b73a741d181cac3b27b93bbf
Kim LaRocca <kim@kimlarocca.com>
2022-07-12 16:50:42 -0400

    Added stuff back into Author.ts

--- composables/types/Author.ts
+++ composables/types/Author.ts
@@ -1,9 +1,18 @@
-// import ContributingOrganization from "./ContributingOrganization";
+import ContributingOrganization from "./ContributingOrganization";
 
 export default interface Author {
+    id?: number;
     firstName: string;
     lastName: string;
-    organization: string;
-    organizationUrl: string;
-    url: string;
+    organization?: string;
+    organizationUrl?: string;
+    url?: string;
+    name?: string;
+    contributingOrganization?: ContributingOrganization;
+    photoID?: number;
+    jobTitle?: string;
+    biography?: string;
+    website?:string;
+    email?: string;
+    slug?: string;
 }
