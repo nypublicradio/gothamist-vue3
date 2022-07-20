315fb39b7dffb16e6bd159a76a1189acfdd6eb57
walsh9 <walsh9@gmail.com>
2022-07-20 14:26:02 -0400

    update nuxt to rc6

--- composables/useAviary.ts
+++ composables/useAviary.ts
@@ -6,7 +6,10 @@ function transformResponseData(data: Record<string, any>): Record<string, any> {
 
 export default async function useAviary(path: string, options: Record<string, any> = {}) {
     const config = useRuntimeConfig()
-    const {data, error} = await useFetch(path, { baseURL: config['API_URL'], ...options })
+    // manually a generating fetch key here as a workaround for an issue in nuxt3 rc6
+    // https://github.com/nuxt/framework/issues/5993
+    const key = path + options.toString()
+    const {data, error} = await useFetch(path, { baseURL: config['API_URL'], key, ...options })
     const transformedData = transformResponseData(data)
     return { data: transformedData, error }
 }
