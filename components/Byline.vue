a3aa957bc99fe448caa39662a7bae2ef32b00bce
Thomas Bono <thomasbono.com@gmail.com>
2022-07-26 19:06:09 -0400

    starting seperate call on authors

--- components/Byline.vue
+++ components/Byline.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts">
-import { ref, computed, onMounted, defineEmits } from 'vue'
+<script setup>
+import { useRuntimeConfig } from '#app'
+import { ref, onBeforeMount, onMounted } from 'vue'
 import { formatDateAndTime } from '~/utilities/date'
 import VFlexibleLink from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VFlexibleLink.vue'
 
+const config = useRuntimeConfig()
 const props = defineProps({
   article: {
     type: Object,
@@ -10,58 +12,101 @@ const props = defineProps({
   },
 })
 
-const authors = ref(props.article?.authors || [])
+const authors = ref([])
 const isMultipleAuthors = ref(authors.value.length > 1)
-const date = ref(new Date(props.article?.publicationDate) || null)
-const updatedDate = ref(new Date(props.article?.updatedDate) || null)
+const date = ref(props.article?.publicationDate || null)
+const updatedDate = ref(props.article?.updatedDate || null)
 const comments = ref(props.article?.comments || null)
 const isSponsored = ref(props.article?.sponsoredContent || false)
-const sponsors = ref(props.article?.sponsors || [])
+const sponsor = ref(props.article?.sponsors[0] || [])
 
-// lifecycle hooks
-onMounted(() => {})
+const getSponsorImage = (sponsorLogoID) => {
+  //need backend work to get the sponsored image
+  //return sponsorLogoID || '/sponsor-default.jpg'
+  return '/sponsor-default.jpg'
+}
+
+const getAuthors = () => {
+  const authorArray = []
+  props.article.authors.forEach((author, index, arr) => {
+    $fetch(config.API_URL + '/pages/' + author.id).then((response) => {
+      console.log('response = ', response)
+      authorArray.push(response)
+      //const imgSrc = String(response['photo']['meta']['download_url'])
+      // const imgSrc = response.photo?.meta?.download_url || null
+      // console.log('response =', response)
+
+      // if (imgSrc) {
+      //   console.log('imgSrc = ', imgSrc)
+      //   return imgSrc
+      // } else {
+      //   console.log('avatar')
+      //   return '/avatar.svg'
+      // }
+    })
+  })
+  authors.value = authorArray
+}
+
+onBeforeMount(() => {
+  getAuthors()
+})
+
+onMounted(() => {
+  console.log('authors data =', authors.value)
+})
 </script>
 
 <template>
   <div class="byline">
     <hr class="black" />
+
+    <!-- sponsored -->
+    <div v-if="isSponsored" class="sponsored flex align-items-center py-4">
+      <div class="author-image mr-3">
+        <v-flexible-link :to="sponsor.link" raw>
+          <img
+            :src="getSponsorImage(sponsor.logo) || '/avatar.svg'"
+            alt="Sponsor's image"
+          />
+        </v-flexible-link>
+      </div>
+      <div>
+        <p class="type-caption">Article sponsored by</p>
+        <v-flexible-link :to="sponsor.link" class="author-name" raw>
+          {{ sponsor.name }}
+        </v-flexible-link>
+      </div>
+    </div>
+    <!-- authors -->
     <div
+      v-else
       class="author flex py-4"
       :class="isMultipleAuthors ? 'multiple-authors' : 'one-author'"
     >
       <div class="author-images flex">
         <div v-for="author of authors" class="author-image mr-3">
           <v-flexible-link :to="author.url" raw>
-            <img :src="author.photoID || '/avatar.svg'" />
+            <img :src="getAuthorImage(author.id)" alt="Author's image" />
           </v-flexible-link>
         </div>
       </div>
       <div>
-        <div>
-          <div v-if="isSponsored" class="sponsored">
-            <p class="type-caption">Article sponsored by</p>
-            <div v-for="sponsor of sponsors" class="author-image mr-3">
-              <v-flexible-link :to="sponsor.url" raw>
-                <img :src="sponsor.photoID || '/avatar.svg'" />
-              </v-flexible-link>
-            </div>
-          </div>
-
-          <div v-else class="author-name">
-            By
-            <span v-for="(author, index) of authors">
-              <v-flexible-link :to="author.url" class="author-name">
-                {{ author.name }}
-              </v-flexible-link>
-              <span
-                v-if="isMultipleAuthors && index !== authors.length - 1"
-                class="author-name"
-              >
-                &nbsp;and&nbsp;
-              </span>
+        <div class="author-name">
+          By
+          <span v-for="(author, index) of authors">
+            <v-flexible-link :to="author.url" class="author-name">
+              {{ author.name }}
+            </v-flexible-link>
+            <span
+              v-if="isMultipleAuthors && index !== authors.length - 1"
+              class="author-name"
+            >
+              &nbsp;and&nbsp;
             </span>
-          </div>
+          </span>
         </div>
+
         <p class="type-caption">Published: {{ formatDateAndTime(date) }}</p>
         <p v-if="updatedDate" class="type-caption">
           Updated: {{ formatDateAndTime(updatedDate) }}
@@ -77,14 +122,14 @@ onMounted(() => {})
 
 <style lang="scss">
 .byline {
-  .author {
-    display: flex;
-    align-items: center;
+  .author,
+  .sponsored {
     .author-image {
       background: #ffffff;
       width: 60px;
       height: 60px;
       border-radius: 50%;
+      overflow: hidden;
       img {
         width: 60px;
         height: 60px;
