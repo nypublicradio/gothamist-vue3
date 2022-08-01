1c8301256d5301eb05e3213b10de3d049cf434bb
Kim LaRocca <kim@kimlarocca.com>
2022-08-01 16:39:20 -0400

    new gallery page!

--- pages/[sectionSlug]/photos/[gallerySlug].vue
+++ pages/[sectionSlug]/photos/[gallerySlug].vue
@@ -1,45 +1,193 @@
 <script setup lang="ts">
-  import { GalleryPage } from '~/composables/types/Page'
-  import VImageWithCaptionVue from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VImageWithCaption.vue';
+import { GalleryPage } from '~/composables/types/Page'
+import VImageWithCaption from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VImageWithCaption.vue'
+import VShareTools from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VShareTools.vue'
+import VShareToolsItem from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VShareToolsItem.vue'
 
-  const route = useRoute()
-  const { $analytics, $htlbid } = useNuxtApp()
-  const gallery = await findPage(`${route.params.sectionSlug}/photos/${route.params.gallerySlug}`)
-    .then(({data}) => normalizeFindPageResponse(data)) as GalleryPage
+const route = useRoute()
+const { $analytics, $htlbid } = useNuxtApp()
 
-  const headMetadata = useGalleryPageHeadMetadata(gallery)
-  const adTargetingData = { Template: 'Article Gallery' }
+const gallery = (await findPage(
+  `${route.params.sectionSlug}/photos/${route.params.gallerySlug}`
+).then(({ data }) => normalizeFindPageResponse(data))) as GalleryPage
 
-  useHead(headMetadata)
+const shareUrl = ref(gallery.url)
+const shareTitle = ref(gallery.title)
 
-  onMounted(() => {
-    $analytics.sendPageView({page_type: 'gallery'})
-    $htlbid.setTargeting(adTargetingData)
-  })
+const headMetadata = useGalleryPageHeadMetadata(gallery)
+const adTargetingData = { Template: 'Article Gallery' }
 
-  onUnmounted(() => {
-    $htlbid.clearTargeting(adTargetingData)
-  })
+useHead(headMetadata)
+
+onMounted(() => {
+  $analytics.sendPageView({ page_type: 'gallery' })
+  $htlbid.setTargeting(adTargetingData)
+})
+
+onUnmounted(() => {
+  $htlbid.clearTargeting(adTargetingData)
+})
 </script>
 
 <template>
-  <div v-if="gallery">
-    <NuxtLink :to="gallery.articleLink">Back</NuxtLink>
-    <h2 v-if="gallery.title">{{ gallery.articleTitle }} - Photos</h2>
-    <div v-for="slide in gallery.slides" :key="slide.image.id">
-      <div>
-        <h3 v-if="slide.title">{{ slide.title }}</h3>
-        <VImageWithCaptionVue
-          :image="useImageUrl(slide.image)"
-          :alt-text="slide.image.alt"
-          :caption="slide.image.caption"
-          :credit="slide.image.credit"
-          :credit-url="slide.image.creditLink"
-          :width="slide.image.width"
-          :height="slide.image.height"
-          :max-width="1200"
-        />
+  <section v-if="gallery" data-style-mode="dark">
+    <div class="content">
+      <div class="grid gutter-30 mb-4">
+        <div class="col-6">
+          <v-share-tools label="Share" class="mt-3">
+            <v-share-tools-item
+              action="share"
+              service="facebook"
+              :url="shareUrl"
+              :utm-parameters="{
+                medium: 'social',
+                source: 'facebook',
+                campaign: 'shared_facebook',
+              }"
+              @share="
+                sendEvent('click_tracking', {
+                  event_category: 'Click Tracking',
+                  component: 'Article Byline',
+                  event_label: 'Social Share Facebook',
+                })
+              "
+            />
+
+            <v-share-tools-item
+              action="share"
+              service="twitter"
+              :url="shareUrl"
+              :share-parameters="{ text: shareTitle, via: 'gothamist' }"
+              :utm-parameters="{
+                medium: 'social',
+                source: 'twitter',
+                campaign: 'shared_twitter',
+              }"
+              @share="
+                sendEvent('click_tracking', {
+                  event_category: 'Click Tracking',
+                  component: 'Article Byline',
+                  event_label: 'Social Share Twitter',
+                })
+              "
+            />
+            <v-share-tools-item
+              action="share"
+              service="reddit"
+              :url="shareUrl"
+              :share-parameters="{ title: shareTitle }"
+              :utm-parameters="{
+                medium: 'social',
+                source: 'reddit',
+                campaign: 'shared_reddit',
+              }"
+              @share="
+                sendEvent('click_tracking', {
+                  event_category: 'Click Tracking',
+                  component: 'Article Byline',
+                  event_label: 'Social Share Reddit',
+                })
+              "
+            />
+            <v-share-tools-item
+              action="share"
+              service="email"
+              :url="shareUrl"
+              :share-parameters="{ body: shareTitle + ' - %URL%' }"
+              :utm-parameters="{
+                medium: 'social',
+                source: 'email',
+                campaign: 'shared_email',
+              }"
+              @share="
+                sendEvent('click_tracking', {
+                  event_category: 'Click Tracking',
+                  component: 'Article Byline',
+                  event_label: 'Social Share Email',
+                })
+              "
+            />
+          </v-share-tools>
+        </div>
+        <div class="col-6 text-right">
+          <NuxtLink :to="gallery.articleLink">
+            <i class="pi pi-times" />
+          </NuxtLink>
+        </div>
+      </div>
+      <div class="grid gutter-30">
+        <div class="col-12">
+          <h1 v-if="gallery.title">{{ gallery.articleTitle }}</h1>
+          <p class="gallery-photo-count mb-3">
+            {{ gallery.slides.length }} photos
+          </p>
+        </div>
+        <template v-for="(slide, index) in gallery.slides" :key="index">
+          <div class="col-12" :class="{ 'xl:col-6': index % 3 !== 0 }">
+            <template v-if="index % 3 === 0 && index !== 0">
+              <img
+                class="mb-4 xl:mb-7"
+                src="https://fakeimg.pl/300x250/?text=AD Here"
+              />
+            </template>
+            <v-image-with-caption
+              :image="useImageUrl(slide.image)"
+              :imageUrl="slide.image.file"
+              :alt-text="slide.image.alt"
+              :maxWidth="slide.image.width"
+              :maxHeight="slide.image.height"
+              :credit="`Photo by ${slide.image.credit}`"
+              :credit-url="slide.image.creditLink"
+              :description="slide.title || slide.image.caption"
+              :quality="80"
+            />
+            <hr class="mt-4 xl:mt-4 mb-2 xl:mb-4" />
+          </div>
+        </template>
+      </div>
+      <div class="text-right mt-2">
+        <NuxtLink :to="gallery.articleLink">
+          <Button class="p-button-rounded" label="Back to article" />
+        </NuxtLink>
       </div>
     </div>
-  </div>
+  </section>
 </template>
+
+<style lang="scss">
+.sectionSlug-photos-gallerySlug header,
+.sectionSlug-photos-gallerySlug .gothamist-footer {
+  display: none;
+}
+
+.sectionSlug-photos-gallerySlug {
+  background: var(--black);
+}
+
+.sectionSlug-photos-gallerySlug .pi-times {
+  font-size: var(--font-size-9);
+}
+
+.sectionSlug-photos-gallerySlug h1 {
+  max-width: 665px;
+  font-size: var(--font-size-14);
+  line-height: var(--font-size-15);
+}
+
+.sectionSlug-photos-gallerySlug .gallery-photo-count {
+  font-size: var(--font-size-4);
+  color: var(--black-100);
+}
+
+.sectionSlug-photos-gallerySlug [data-style-mode='dark'] hr {
+  background: var(--black-400);
+}
+
+.sectionSlug-photos-gallerySlug .image-with-caption-credit-link,
+.sectionSlug-photos-gallerySlug .image-with-caption-credit-link .footer {
+  font-size: var(--font-size-4);
+  color: var(--black-100);
+  font-weight: normal;
+  text-decoration: none;
+}
+</style>
