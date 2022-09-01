1e2d9d2ffe8167cdbf2dd4e066c875c4008fd600
Kim LaRocca <kim@kimlarocca.com>
2022-09-01 09:50:11 -0400

    putting back some stuff that fell off the site (#107)

--- components/CenterFeature.vue
+++ components/CenterFeature.vue
@@ -23,119 +23,119 @@ const articlesSm = ref([
 
 <template>
   <div v-if="articleLg && articleMd && articlesSm" class="center-feature">
-    <div class="grid gutter-x-30">
-      <div class="col-fixed flex-order-2 lg:flex-order-1">
-        <!-- md article desktop  -->
-        <v-card
-          class="hidden lg:flex article-md mod-vertical mod-large mb-5 tag-small"
-          :image="useImageUrl(articleMd.listingImage)"
-          :title="articleMd.listingTitle"
-          :titleLink="articleMd.link"
-          :ratio="[3, 2]"
-          :width="433"
-          :height="289"
-          :sizes="[1]"
-          :maxWidth="articleMd.listingImage?.width"
-          :maxHeight="articleMd.listingImage?.height"
-          loading="eager"
-          :tags="[
-            {
-              name: articleMd.section.name,
-              slug: `/${articleMd.section.slug}`,
-            },
-          ]"
-        >
-          <p>
-            {{ articleMd.description }}
-          </p>
-          <v-card-metadata stack :article="articleMd" />
-        </v-card>
-        <!-- md article mobile  -->
-        <v-card
-          class="flex lg:hidden article-md mod-horizontal mod-left tag-small"
-          :image="useImageUrl(articleMd.listingImage)"
-          :title="articleMd.listingTitle"
-          :titleLink="articleMd.link"
-          :width="318"
-          :height="212"
-          :sizes="[1]"
-          :maxWidth="articleMd.listingImage?.width"
-          :maxHeight="articleMd.listingImage?.height"
-          loading="eager"
-          :tags="[
-            {
-              name: articleMd.section.name,
-              slug: `/${articleMd.section.slug}`,
-            },
-          ]"
-        >
-          <p>
-            {{ articleMd.description }}
-          </p>
-          <v-card-metadata :article="articleMd" />
-        </v-card>
-        <div class="hidden lg:block mb-4 xl:mb-7">
-          <HtlAd
-            layout="rectangle"
-            slot="htlad-gothamist_index_topper"
-          />
-        </div>
-      </div>
-      <div class="col flex-order-1 lg:flex-order-2">
-        <v-card
-          class="article-lg mod-vertical mod-large mb-4"
-          :image="useImageUrl(articleLg.listingImage)"
-          :sizes="[1]"
-          :width="897"
-          :height="598"
-          :title="articleLg.listingTitle"
-          :titleLink="articleLg.link"
-          :maxWidth="articleLg.listingImage?.width"
-          :maxHeight="articleLg.listingImage?.height"
-          loading="eager"
-          :tags="[
-            {
-              name: articleLg.section.name,
-              slug: `/${articleLg.section.slug}`,
-            },
-          ]"
-        >
-          <v-card-metadata
-            class="mt-0 md:mt-2"
-            altDesign
-            :article="articleLg"
-          />
-        </v-card>
-        <hr class="block lg:hidden mb-3" />
-      </div>
-      <div class="col-3 flex-order-3">
-        <hr class="my-3 block xl:hidden" />
-        <horizontal-drag :articles="articlesSm" v-slot="slotProps">
+    <div class="content">
+      <template v-if="collection.label">
+        <hr class="mb-2 black" />
+        <div class="type-label3 mb-5">{{ collection.label }}</div>
+      </template>
+      <div class="grid gutter-x-30">
+        <div class="col-fixed flex-order-2 lg:flex-order-1">
+          <!-- md article desktop  -->
           <v-card
-            class="mod-horizontal mod-left mod-small mb-0"
-            :image="useImageUrl(slotProps.article.listingImage)"
-            :width="106"
-            :height="106"
-            :ratio="[1, 1]"
-            :sizes="[2]"
-            :title="slotProps.article.listingTitle || slotProps.article.title"
-            :titleLink="slotProps.article.link"
-            :maxWidth="slotProps.article.listingImage?.width"
-            :maxHeight="slotProps.article.listingImage?.height"
-            :quality="80"
+            class="hidden lg:flex article-md mod-vertical mod-large mb-5 tag-small"
+            :image="useImageUrl(articleMd.listingImage)"
+            :title="articleMd.listingTitle"
+            :titleLink="articleMd.link"
+            :ratio="[3, 2]"
+            :width="433"
+            :height="289"
+            :sizes="[1]"
+            :maxWidth="articleMd.listingImage?.width"
+            :maxHeight="articleMd.listingImage?.height"
+            loading="eager"
+            :tags="[
+              {
+                name: articleMd.section.name,
+                slug: `/${articleMd.section.slug}`,
+              },
+            ]"
           >
-            <div></div>
+            <p>
+              {{ articleMd.description }}
+            </p>
+            <v-card-metadata stack :article="articleMd" />
+          </v-card>
+          <!-- md article mobile  -->
+          <v-card
+            class="flex lg:hidden article-md mod-horizontal mod-left tag-small"
+            :image="useImageUrl(articleMd.listingImage)"
+            :title="articleMd.listingTitle"
+            :titleLink="articleMd.link"
+            :width="318"
+            :height="212"
+            :sizes="[1]"
+            :maxWidth="articleMd.listingImage?.width"
+            :maxHeight="articleMd.listingImage?.height"
+            loading="eager"
+            :tags="[
+              {
+                name: articleMd.section.name,
+                slug: `/${articleMd.section.slug}`,
+              },
+            ]"
+          >
+            <p>
+              {{ articleMd.description }}
+            </p>
+            <v-card-metadata :article="articleMd" />
+          </v-card>
+          <div class="hidden lg:block mb-4 xl:mb-7">
+            <HtlAd layout="rectangle" slot="htlad-gothamist_index_topper" />
+          </div>
+        </div>
+        <div class="col flex-order-1 lg:flex-order-2">
+          <v-card
+            class="article-lg mod-vertical mod-large mb-4"
+            :image="useImageUrl(articleLg.listingImage)"
+            :sizes="[1]"
+            :width="897"
+            :height="598"
+            :title="articleLg.listingTitle"
+            :titleLink="articleLg.link"
+            :maxWidth="articleLg.listingImage?.width"
+            :maxHeight="articleLg.listingImage?.height"
+            loading="eager"
+            :tags="[
+              {
+                name: articleLg.section.name,
+                slug: `/${articleLg.section.slug}`,
+              },
+            ]"
+          >
             <v-card-metadata
-              :article="slotProps.article"
-              :showComments="false"
+              class="mt-0 md:mt-2"
+              altDesign
+              :article="articleLg"
             />
           </v-card>
-        </horizontal-drag>
-        <div class="block lg:hidden mb-4 xl:mb-7 m-auto mt-6">
-          <HtlAd
-            layout="rectangle"
-            slot="htlad-gothamist_index_topper"
-          />
+          <hr class="block lg:hidden mb-3" />
+        </div>
+        <div class="col-3 flex-order-3">
+          <hr class="my-3 block xl:hidden" />
+          <horizontal-drag :articles="articlesSm" v-slot="slotProps">
+            <v-card
+              class="mod-horizontal mod-left mod-small mb-0"
+              :image="useImageUrl(slotProps.article.listingImage)"
+              :width="106"
+              :height="106"
+              :ratio="[1, 1]"
+              :sizes="[2]"
+              :title="slotProps.article.listingTitle || slotProps.article.title"
+              :titleLink="slotProps.article.link"
+              :maxWidth="slotProps.article.listingImage?.width"
+              :maxHeight="slotProps.article.listingImage?.height"
+              :quality="80"
+            >
+              <div></div>
+              <v-card-metadata
+                :article="slotProps.article"
+                :showComments="false"
+              />
+            </v-card>
+          </horizontal-drag>
+          <div class="block lg:hidden mb-4 xl:mb-7 m-auto mt-6">
+            <HtlAd layout="rectangle" slot="htlad-gothamist_index_topper" />
+          </div>
         </div>
       </div>
     </div>
