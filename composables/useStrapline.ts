d5110fd7183450e66d7e48cd13405f841590416d
Thomas Bono <thomasbono.com@gmail.com>
2022-09-07 16:57:44 -0400

    matt updates

--- composables/useStrapline.ts
+++
@@ -1,5 +0,0 @@
-
-export default async function useStrapline() {
-    const homePageData = await usePageById(3)
-    return await homePageData.data.value?.strapline || "A non-profit newsroom, powered by WNYC."
-}
