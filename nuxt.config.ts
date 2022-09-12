894640094dcaaaa29c1d145c9f6f0f16392061c7
Thomas Bono <thomasbono.com@gmail.com>
2022-09-12 12:02:35 -0400

    primeVue and DS optimizations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,7 @@ export default defineNuxtConfig({
       preprocessorOptions: {
         scss: {
           // besure to mirror theses imports in the vitest.config.ts
-          additionalData: `@import "@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/variables.scss"; @import "@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/_mixins.scss"; @import "@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/typography.scss"; @import "~/assets/scss/global.scss";`,
+          additionalData: `@import "@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/breakpoints.scss"; @import "@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/_mixins.scss"; @import "@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/typography.scss"; @import "~/assets/scss/global.scss";`,
         },
       },
       postcss: {
