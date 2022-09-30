4e6d6b6afb87bd0870e56e1e7c6eca0a9dc6f101
walsh9 <walsh9@gmail.com>
2022-09-30 17:54:24 -0400

    cleanup

--- composables/useAviary.ts
+++ composables/useAviary.ts
@@ -5,7 +5,7 @@ function transformResponseData(data: Record<string, any>): Record<string, any> {
     return humps.camelizeKeys(data)
 }
 
-export default async function useAviary(path: string, options: Record<string, any> = {}, refresh: boolean = true) {
+export default async function useAviary(path: string, options: Record<string, any> = {}, refresh = true) {
     const config = useRuntimeConfig()
     // Sets a timestamp to include in the values used to create the
     // fetch key, to force a refresh of the local caching
@@ -13,7 +13,6 @@ export default async function useAviary(path: string, options: Record<string, an
     // manually a generating fetch key here as a workaround for an issue in nuxt3 rc6
     // https://github.com/nuxt/framework/issues/5993
     const key = hash(['aviary', path, options, refreshTimestamp])
-    console.log('fetch', path, options, key)
     const { data, error } = await useFetch(path, { baseURL: config['API_URL'], key, ...options })
     const transformedData = transformResponseData(data)
     return { data: transformedData, error }
