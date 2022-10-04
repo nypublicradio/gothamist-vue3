4a4784d82c45d893ccbbeb85cd6db12123919b91
walsh9 <walsh9@gmail.com>
2022-10-04 18:11:34 -0400

    Add grubhub sponsor placement

---
+++ components/SponsorPlacement.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="sponsor-placement flex mb-3">
+        <div class="sponsor-placement-logo pr-3">
+            <img alt="GrubHub Logo" 
+            src="/grubhub-circle.png"
+            srcset="/grubhub-circle.png, /grubhub-circle2x.png 2x, /grubhub-circle4x.png 4x"
+            width=64
+            height=64
+            />
+        </div>
+        <div class="sponsor-placement-details flex flex-column">
+        <p>Sponsored by</p>
+        <p class="sponsor-name">Grubhub</p>
+        <p>a proud supporter of Gothamist</p>
+        </div>
+    </div>
+</template>
+<style lang="scss">
+    .sponsor-placement p {
+        font-family: var(--font-family-header);
+        font-size: var(--font-size-5);
+        line-height: 1.5;
+    }
+    .sponsor-name {
+        font-weight: bold;
+    }
+</style>
