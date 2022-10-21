9c9898d925f82d0e620c84d98a6ba808ae30b429
walsh9 <walsh9@gmail.com>
2022-10-21 18:26:19 -0400

    use send_to param to send page views to npr ga

--- plugins/analytics.client.ts
+++ plugins/analytics.client.ts
@@ -10,19 +10,31 @@ export default defineNuxtPlugin(() => {
   gtag('js', new Date())
   gtag('set', 'send_page_view', false)
   gtag('config', config.GA_MEASUREMENT_ID)
+  gtag('config', config.NPR_GA_MEASUREMENT_ID)
 
   // event to use when sending gtag events
   const sendEvent = (name: string, params: Record<string, string>) => {
-    gtag('event', name, params)
+    gtag('event', name, {
+      send_to:  config.GA_MEASUREMENT_ID,
+      ...params
+    })
   }
   // gtag even for reporting on page views
   const sendPageView = (params: Record<string, string>) => {
     sendEvent('page_view', {
+      send_to:  config.GA_MEASUREMENT_ID,
       page_location: document.location.href,
       page_title: document.title,
       NYPRMember: membershipStatus.value,
       ...params
     })
+    if (config.NPR_GA_MEASUREMENT_ID) {
+      sendEvent('page_view', {
+        send_to:  config.NPR_GA_MEASUREMENT_ID,
+        page_location: document.location.href,
+        page_title: document.title,
+      })
+    }
   }
   return {
     provide: {
