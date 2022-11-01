e837c03c8561be1dc90f2ec7a70efb38a07601b3
walsh9 <walsh9@gmail.com>
2022-11-01 18:54:05 -0400

    lets forget about router options for now and do it all in our scroll plugin

--- plugins/scrollToTop.js
+++ plugins/scrollToTop.js
@@ -1,9 +1,14 @@
 import { defineNuxtPlugin } from '#app'
-// this will always bring the user to the top of the page. Nuxt3 will hopfully have a better solution soon for this where we can use saved positions when going back etc...
+
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.hook('page:finish', () => {
-        if (!document.location.hash) {
-            window.scrollTo(0, 0)
+    nuxtApp.hook('page:finish', (a,b,c) => {
+        if (document.location.hash) {
+            const scrollTarget =  document.querySelector(document.location.hash)
+            if (scrollTarget) {
+                scrollTarget.scrollIntoView()
+                return
+            }
         }
+        window.scrollTo(0, 0)
     })
 })
