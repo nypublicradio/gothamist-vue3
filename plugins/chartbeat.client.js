fd982a24d0aa257d3d8eda4adf2d5a8e40f6a067
Kim LaRocca <kim@kimlarocca.com>
2022-11-07 11:54:54 -0500

    deepsource

--- plugins/chartbeat.client.js
+++ plugins/chartbeat.client.js
@@ -2,7 +2,7 @@ export default defineNuxtPlugin(() => {
   /* eslint-disable */
   (function() {
     /** CONFIGURATION START **/
-    var _sf_async_config = window._sf_async_config = (window._sf_async_config || {});
+    let _sf_async_config = window._sf_async_config = (window._sf_async_config || {});
     _sf_async_config.uid = 16246; //CHANGE THIS TO YOUR ACCOUNT ID
     _sf_async_config.domain = 'gothamist.com'; //CHANGE THIS TO YOUR SITE ID
     _sf_async_config.useCanonical = true;
@@ -11,8 +11,8 @@ export default defineNuxtPlugin(() => {
     _sf_async_config.authors = ''; //SET PAGE AUTHOR(S)
     /** CONFIGURATION END **/
     function loadChartbeat() {
-      var e = document.createElement('script');
-      var n = document.getElementsByTagName('script')[0];
+      let e = document.createElement('script');
+      let n = document.getElementsByTagName('script')[0];
       e.type = 'text/javascript';
       e.async = true;
       e.src = '//static.chartbeat.com/js/chartbeat.js';
