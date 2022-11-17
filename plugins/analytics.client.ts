4a74f75b51eb674e2202fa0740e8563fc7ae3347
walsh9 <walsh9@gmail.com>
2022-11-17 16:13:31 -0500

    use groups syntax

--- plugins/analytics.client.ts
+++ plugins/analytics.client.ts
@@ -9,28 +9,35 @@ export default defineNuxtPlugin(() => {
   function gtag() { dataLayer.push(arguments) }
   gtag('js', new Date())
   gtag('set', 'send_page_view', false)
-  gtag('config', config.GA_MEASUREMENT_ID)
-  gtag('config', config.NPR_GA_MEASUREMENT_ID)
 
-  // event to use when sending gtag events
+  // Add multiple measurement IDs so we can route events
+  // to them using named groups and the 'send_to' parameter
+  // https://developers.google.com/tag-platform/gtagjs/routing
+  gtag('config', config.GA_MEASUREMENT_ID, { 'groups': 'NYPR' })
+  gtag('config', config.NPR_GA_MEASUREMENT_ID, { 'groups': 'NPR' })
+
+  // send a custom named gtag events
   const sendEvent = (name: string, params: Record<string, string>) => {
     gtag('event', name, {
-      send_to:  config.GA_MEASUREMENT_ID,
+      send_to:  'NYPR',
       ...params
     })
   }
-  // gtag even for reporting on page views
+
+  // send a page view event
   const sendPageView = (params: Record<string, string>) => {
+    // send a page view event to our main GA property
     sendEvent('page_view', {
-      send_to:  config.GA_MEASUREMENT_ID,
+      send_to:  'NYPR',
       page_location: document.location.href,
       page_title: document.title,
       NYPRMember: membershipStatus.value,
       ...params
     })
+    // also send a page view to NPR (we dont need our member status flag here or extra params here)
     if (config.NPR_GA_MEASUREMENT_ID) {
       sendEvent('page_view', {
-        send_to:  config.NPR_GA_MEASUREMENT_ID,
+        send_to: 'NPR',
         page_location: document.location.href,
         page_title: document.title,
       })
