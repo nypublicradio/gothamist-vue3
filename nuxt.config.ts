4f6c0adfb2516c53e693ca6ef5ec148ffd666c80
walsh9 <walsh9@gmail.com>
2022-12-15 13:18:38 -0500

    update config meta to use new app.head api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,33 @@
 export default defineNuxtConfig({
-  meta: {
-    meta: [
-      { name: 'charset', content: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'msapplication-TileColor', content: '#f4f2f0' },
-      { name: 'theme-color', content: '#f4f2f0' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'apple-touch-icon', sizes: "180x180", href: '/apple-touch-icon.png' },
-      { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#ff3904' },
-      {
-        rel: 'preconnect',
-        href: process.env.API_URL,
-        crossorigin: true
-      },
-      {
-        rel: 'preconnect',
-        href: 'https://www.googletagmanager.com',
-        crossorigin: true
-      },
-      {
-        rel: 'preconnect',
-        href: 'https://www.google-analytics.com',
-        crossorigin: true
-      }
-    ],
+  app: {
+    head: {
+      meta: [
+        { name: 'msapplication-TileColor', content: '#f4f2f0' },
+        { name: 'theme-color', content: '#f4f2f0' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: "180x180", href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#ff3904' },
+        {
+          rel: 'preconnect',
+          href: process.env.API_URL,
+          crossorigin: ''
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://www.googletagmanager.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://www.google-analytics.com',
+          crossorigin: ''
+        }
+      ],
+    }
   },
   css: [
     '@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/fonts/fonts.css',
