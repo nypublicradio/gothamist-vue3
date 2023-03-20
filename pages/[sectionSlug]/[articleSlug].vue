f3b9a6c4cbf5ab4c320a4f064a595fd0801d1ffe
walsh9 <walsh9@gmail.com>
2023-03-20 16:53:39 -0400

    cleanup

--- pages/[sectionSlug]/[articleSlug].vue
+++ pages/[sectionSlug]/[articleSlug].vue
@@ -106,7 +106,7 @@ const showMarketingBanner = computed(() => {
 })
 
 const tagName = computed(() => article?.sponsoredContent ? "Sponsored" : article?.section?.name )
-const tagSlug = computed(() => article?.sponsoredContent ? "" : article?.section?.slug )
+const tagSlug = computed(() => article?.sponsoredContent ? "" : `/${article?.section?.slug}` )
 </script>
 <template>
   <div>
@@ -132,7 +132,7 @@ const tagSlug = computed(() => article?.sponsoredContent ? "" : article?.section
               v-if="tagName"
               :class="article?.sponsoredContent && 'sponsored'"
               :name="tagName"
-              :slug="`/${tagSlug}`"
+              :slug="tagSlug"
             />
             <h1 class="mt-4 mb-3 h2">{{ article.title }}</h1>
           </div>
