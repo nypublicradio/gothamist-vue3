2ce97ce347055baf6338ec1c5a4a8b540c93e99e
walsh9 <walsh9@gmail.com>
2023-04-13 11:20:05 -0400

    changes for deepsource

---
+++ composables/data/basePages.ts
@@ -0,0 +1,29 @@
+import { Page } from "../types/Page"
+
+export const WAGTAIL_PAGE_TYPES = {
+    "gallery.GalleryPage": 'gallery',
+    "news.ArticlePage": 'article_page',
+    "standardpages.IndexPage": 'section_page',
+    "standardpages.InformationPage": 'information_page',
+    "tagpages.TagPage": 'tag_page'
+}
+
+export function normalizePage(page: Record<string, any>): Page {
+    return {
+        id: page.id,
+        title: page.title,
+        uuid: page.uuid,
+        type: WAGTAIL_PAGE_TYPES[page.meta?.type] || 'unknown',
+    
+        listingTitle: page.listingTitle || page.title,
+        listingDescription: page.listingSummary || page.description,
+        listingImage: page.listingImage,
+    
+        socialTitle: page.socialTitle || page.title,
+        socialDescription: page.socialText || page.description,
+        socialImage: page.socialImage,
+    
+        seoTitle: page.meta?.seoTitle || page.title,
+        searchDescription: page.meta?.searchDescription || page.title
+    }
+}
