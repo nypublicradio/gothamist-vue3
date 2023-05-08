1469388c70c5f8a152fe2b2d6f0515ccb7dd75ef
walsh9 <walsh9@gmail.com>
2023-05-08 17:07:07 -0400

    remove mystery optimization (don't know if it does anything so leave it for now)

--- layouts/default.vue
+++ layouts/default.vue
@@ -144,7 +144,6 @@ useHead({
         js = d.createElement(s);
         js.id = id;
         js.src = 'https://platform.twitter.com/widgets.js';
-        js.setAttribute('async', '')
         fjs.parentNode.insertBefore(js, fjs);
 
         t._e = [];
