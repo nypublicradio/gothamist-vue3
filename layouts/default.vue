84962cb68578dcc72225dcd30e2cf6db69ff1da2
walsh9 <walsh9@gmail.com>
2023-05-11 18:11:36 -0400

    defer loading of twitter script

--- layouts/default.vue
+++ layouts/default.vue
@@ -139,22 +139,27 @@ useHead({
       async: true,
     },
     {
-      children: `window.twttr = (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-          t = window.twttr || {};
-        if (d.getElementById(id)) return t;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = 'https://platform.twitter.com/widgets.js';
-        fjs.parentNode.insertBefore(js, fjs);
+      children: `
+      addEventListener("DOMContentLoaded", () =>
+        {
+          window.twttr = (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0],
+            t = window.twttr || {};
+          if (d.getElementById(id)) return t;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = 'https://platform.twitter.com/widgets.js';
+          fjs.parentNode.insertBefore(js, fjs);
 
-        t._e = [];
-        t.ready = function(f) {
-          t._e.push(f);
-        };
+          t._e = [];
+          t.ready = function(f) {
+            t._e.push(f);
+          };
 
-        return t;
-      }(document, 'script', 'twitter-wjs'));`,
+          return t;
+        }(document, 'script', 'twitter-wjs'));
+        {});
+      })`,
     },
   ],
   noscript: [
