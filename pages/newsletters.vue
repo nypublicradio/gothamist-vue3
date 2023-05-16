1a786e4aae2a3373461cdfaa540762f38f998418
walsh9 <walsh9@gmail.com>
2023-05-16 15:22:44 -0400

    Split out gothamist newsletters

--- pages/newsletters.vue
+++ pages/newsletters.vue
@@ -3,9 +3,13 @@ const config = useRuntimeConfig()
 const { $sentry } = useNuxtApp()
 const newsletters = [
   {
-    title: "Gothamist Daily",
-    id: "Gothamist++Gothamist - Early Addition",
-    description: "Start and end your day with local headlines and fun tidbits from around the internet."
+    title: "Early Addition",
+    id: "Gothamist - Early Addition",
+    description: "Start your weekdays with a guide of the local stories you need to know, plus tidbits from around the internet."
+  },{
+    title: "Evening Headlines",
+    id: "Gothamist",
+    description: "Get links to our top stories delivered to your inbox every day at 5 p.m."
   },{
     title: "We the Commuters",
     id: "We The Commuters",
