cb1a807ec890a8f26071b01e668e943429571f6b
walsh9 <walsh9@gmail.com>
2023-05-19 19:53:45 -0400

    remove unused code

--- composables/useAviary.ts
+++ composables/useAviary.ts
@@ -1,11 +1,10 @@
 import humps from 'humps'
-import { hash } from 'ohash'
 
 export function transformResponseData(data: Record<string, any>): Record<string, any> {
     return humps.camelizeKeys(data)
 }
 
-export default async function useAviary(path: string, options: Record<string, any> = {}, refresh = true) {
+export default async function useAviary(path: string, options: Record<string, any> = {}) {
     const config = useRuntimeConfig()
     const { data, error } = await useFetch(path, { baseURL: config.public['API_URL'], ...options })
     const transformedData = transformResponseData(data)
