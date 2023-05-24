0c2d34417d6e5e85801609a37f6fbd6f55b7c33b
walsh9 <walsh9@gmail.com>
2023-05-24 18:12:57 -0400

    move this to a composable

---
+++ composables/useOptinMonster.ts
@@ -0,0 +1,10 @@
+export default async function useOptinMonster() {
+  onMounted(() => {
+    const config = useRuntimeConfig()
+    if (!process.server && window[`om${config.OPTIN_MONSTER_ACCOUNT_ID}_${config.OPTIN_MONSTER_USER_ID}`]) {
+      const optinMonster = window[`om${config.OPTIN_MONSTER_ACCOUNT_ID}_${config.OPTIN_MONSTER_USER_ID}`]
+      optinMonster.reset()
+      optinMonster.campaigns?.Sessions?.init();
+    }
+  }
+}
