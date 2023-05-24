818b278db7c602cda24d38ff9c0c5bd6770569cb
walsh9 <walsh9@gmail.com>
2023-05-24 16:34:41 -0400

    try moving to onmounted

--- layouts/default.vue
+++ layouts/default.vue
@@ -95,6 +95,11 @@ onMounted(() => {
     })
     $htlbid.setTargetingForRoute(route)
   }
+  if (!process.server && window[`om${config.OPTIN_MONSTER_ACCOUNT_ID}_${config.OPTIN_MONSTER_USER_ID}`]) {
+      const optinMonster = window[`om${config.OPTIN_MONSTER_ACCOUNT_ID}_${config.OPTIN_MONSTER_USER_ID}`]
+      optinMonster.reset()
+      optinMonster.campaigns?.Sessions?.init();
+  }
 })
 watch(route, (value) => {
   if (typeof $htlbid !== "undefined") {
