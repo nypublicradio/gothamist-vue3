e5fe2f446cc3305f66220e7dd561278ce07c1cdf
walsh9 <walsh9@gmail.com>
2023-05-24 16:58:33 -0400

    try moving from layout template to page template

--- pages/[sectionSlug]/[articleSlug].vue
+++ pages/[sectionSlug]/[articleSlug].vue
@@ -84,6 +84,11 @@ onMounted(() => {
   $htlbid.setTargeting(adTargetingData)
   sensitiveContent.value = article.sensitiveContent
   useUpdateCommentCounts([article])
+  if (!process.server && window[`om${config.OPTIN_MONSTER_ACCOUNT_ID}_${config.OPTIN_MONSTER_USER_ID}`]) {
+      const optinMonster = window[`om${config.OPTIN_MONSTER_ACCOUNT_ID}_${config.OPTIN_MONSTER_USER_ID}`]
+      optinMonster.reset()
+      optinMonster.campaigns?.Sessions?.init();
+  }
 })
 
 onUnmounted(() => {
