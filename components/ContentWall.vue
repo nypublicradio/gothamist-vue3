7ffc489a60f4fa5c1a78f56d7ccf9d4e85f3295f
walsh9 <walsh9@gmail.com>
2023-06-01 17:58:45 -0400

    add generic content wall component

---
+++ components/ContentWall.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const showContent = ref(false)
+const dismiss = () => {
+  showContent.value = true
+}
+</script>
+
+<template>
+  <div>
+    <slot v-if="showContent" name="full" />
+    <slot v-if="!showContent" name="leadin" />
+    <slot v-if="!showContent" name="wall" :dismiss="dismiss" />
+  </div>
+</template>
+
+<style lang="scss">
+</style>
