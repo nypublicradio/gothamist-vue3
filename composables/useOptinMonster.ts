9a8a91ceb00a01d064de43f9c54963f260ea6b35
walsh9 <walsh9@users.noreply.github.com>
2023-06-22 12:29:13 -0400

    read config using updated syntax (#292)

--- composables/useOptinMonster.ts
+++ composables/useOptinMonster.ts
@@ -1,8 +1,8 @@
 export default function useOptinMonster() {
   onMounted(() => {
     const config = useRuntimeConfig()
-    if (!process.server && window[`om${config.OPTIN_MONSTER_ACCOUNT_ID}_${config.OPTIN_MONSTER_USER_ID}`]) {
-      const optinMonster = window[`om${config.OPTIN_MONSTER_ACCOUNT_ID}_${config.OPTIN_MONSTER_USER_ID}`]
+    if (!process.server && window[`om${config.public.OPTIN_MONSTER_ACCOUNT_ID}_${config.public.OPTIN_MONSTER_USER_ID}`]) {
+      const optinMonster = window[`om${config.public.OPTIN_MONSTER_ACCOUNT_ID}_${config.public.OPTIN_MONSTER_USER_ID}`]
       optinMonster.reset()
       optinMonster.campaigns?.Sessions?.init();
     }
