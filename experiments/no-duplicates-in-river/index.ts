3a53326eac570857d321d17a0510cfc4c8c0903f
walsh9 <walsh9@users.noreply.github.com>
2023-06-27 14:48:00 -0400

    set new cookie for ab test (#290)

--- experiments/no-duplicates-in-river/index.ts
+++ experiments/no-duplicates-in-river/index.ts
@@ -1,6 +1,6 @@
 export default {
     // A helper exp-{name}-{var} class will be added to the root element
-    name: 'no-duplicates-in-river',
+    name: 'homepage-river',
 
     maxAgeSeconds: 60 * 60 * 24 * 30, //30 days
 
