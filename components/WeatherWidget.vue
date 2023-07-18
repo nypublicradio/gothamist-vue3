2207ebdb2b2353d3933cd027403c542792ef6a22
walsh9 <walsh9@gmail.com>
2023-07-18 18:05:34 -0400

    add unit (F) to temperature in weather widget

--- components/WeatherWidget.vue
+++ components/WeatherWidget.vue
@@ -5,7 +5,8 @@ const { data: weather, pending, error } = await useFetch(`/api/weather`, {server
   <div class="weather-widget">
     <div v-if="!pending && !error && weather">
       <i aria-hidden="true" class="wi" :class="weather.iconName"></i>
-      <span class="sr-only">Current Conditions In NYC: {{weather.textDescription}}. Current Temperature:</span> {{weather.temperature}}°
+      <span class="sr-only">Current Conditions In NYC: {{weather.textDescription}}.</span>
+      <span v-if="weather.temperature"><span class="sr-only">Current Temperature:</span> {{weather.temperature}}°F</span>
     </div>
   </div>
 </template>
