1c18d260436fc6d02c811ede3a061c4ed62491fa
walsh9 <walsh9@gmail.com>
2023-08-03 18:37:51 -0400

    only lock on the client side to prevent weird caching side effects

--- composables/useWalledState.ts
+++ composables/useWalledState.ts
@@ -1,19 +1,20 @@
 import { ArticlePage } from "./types/Page";
 import { compareAsc, subMonths } from 'date-fns';
 export default function useWalledState(article:ArticlePage) {
+  if (!process.server) {
+    // articles newer than the threshold date shouldn't be walled
+    const wallThreshold = subMonths(new Date(), 6) // 6 months ago
+    if (compareAsc(article.updatedDate || article.publicationDate, wallThreshold) > 0) {
+      return false
+    }
 
-  // articles newer than the threshold date shouldn't be walled
-  const wallThreshold = subMonths(new Date(), 6) // 6 months ago
-  if (compareAsc(article.updatedDate || article.publicationDate, wallThreshold) > 0) {
-    return false
+    // sponsored content shouldn't be walled
+    if (article.sponsoredContent) {
+      return false
+    }
+
+    // content shouldn't be walled when the user has a newsletter member cookie
+    const cookie = useCookie('__gothamistNewsletterMember', { path: '/' })
+    return typeof cookie.value === 'undefined'
   }
-
-  // sponsored content shouldn't be walled
-  if (article.sponsoredContent) {
-    return false
-  }
-
-  // content shouldn't be walled when the user has a newsletter member cookie
-  const cookie = useCookie('__gothamistNewsletterMember', { path: '/' })
-  return typeof cookie.value === 'undefined'
 }
