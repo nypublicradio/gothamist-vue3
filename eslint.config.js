83ee3a9c876905ddd43346c4085701862205b5b9
walsh9 <walsh9@gmail.com>
2023-10-12 10:51:28 -0400

    make rules stricter to match deepsource

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,48 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+  typescript: true,
+  vue: true,
   ignores: [
     'api.json',
     'assets/gsap/**',
     'cf_functions/**',
     'cypress/fixtures/**',
   ],
+  typescript: {
+    tsconfigPath: './tsconfig.json',
+    parserOptions: {
+      extraFileExtensions: ['.vue'],
+    },
+  },
   overrides: {
     typescript: {
+      'consistent-return': 'error',
+      'no-implicit-coercion': 'error',
       'node/prefer-global/process': ['error', 'always'],
+      'require-await': 'error',
+      'ts/no-unnecessary-boolean-literal-compare': 'error',
+      'ts/prefer-function-type': 'error',
+      'ts/prefer-optional-chain': 'error',
+      'ts/unified-signatures': 'error',
+
+      // it would be nice to turn these on one day but one step at a time
+      'ts/no-floating-promises': 'off',
+      'ts/no-unnecessary-type-assertion': 'off',
+      'ts/no-unsafe-argument': 'off',
+      'ts/no-unsafe-assignment': 'off',
+      'ts/no-unsafe-call': 'off',
+      'ts/no-unsafe-member-access': 'off',
+      'ts/no-unsafe-return': 'off',
     },
+    vue: {
+      'vue/no-duplicate-attr-inheritance': 'error',
+      'vue/no-empty-component-block': 'error',
+      'vue/no-unused-properties': 'error',
+    },
+  },
+}, {
+  rules: {
+    'style/quotes': ['error', 'single', { allowTemplateLiterals: false, avoidEscape: false }],
   },
 })
