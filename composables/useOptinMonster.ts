7230eb709d6d8efb3c994280e4d32ee9ce07a436
walsh9 <walsh9@gmail.com>
2023-10-19 10:41:40 -0400

    style fixes

--- composables/useOptinMonster.ts
+++ composables/useOptinMonster.ts
@@ -5,14 +5,14 @@ export default function useOptinMonster() {
       children:
       `document.addEventListener('om.Scripts.init', function(event) {
         event.detail.Scripts.enabled.fonts = false;
-      })`
+      })`,
     }],
     style: [{
       children: `html .Campaign .gothamist-sans-serif, html .Campaign .gothamist-sans-serif *
       { font-family: "Haffer SQ", sans-serif !important; }
       html .Campaign .gothamist-serif, html .Campaign .gothamist-serif *
-      { font-family: "Reckless Neue", serif !important; }`
-    }]
+      { font-family: "Reckless Neue", serif !important; }`,
+    }],
   })
   onMounted(() => {
     if (!process.server && window[`om${config.public.OPTIN_MONSTER_ACCOUNT_ID}_${config.public.OPTIN_MONSTER_USER_ID}`]) {
