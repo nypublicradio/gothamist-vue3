19c6e051d1a1cca41cd0ce85bdc4f4d202d3f561
walsh9 <walsh9@gmail.com>
2023-10-26 16:04:55 -0400

    try test splitting

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'cypress'
+import cypressSplit from 'cypress-split'
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+      return config
+    },
   },
 })
