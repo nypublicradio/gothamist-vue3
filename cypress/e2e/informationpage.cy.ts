68908d3762f16bde314aba612cbf59bddd7aa646
walsh9 <walsh9@gmail.com>
2023-11-06 11:32:47 -0500

    update information page tests

--- cypress/e2e/informationpage.cy.ts
+++ cypress/e2e/informationpage.cy.ts
@@ -1,4 +1,6 @@
-describe('An article page', () => {
+import { cypressConfig } from '../support/config'
+
+describe('An information page', () => {
   beforeEach(() => {
     cy.loadGlobalFixtures()
   })
@@ -12,5 +14,16 @@ describe('An article page', () => {
 
     cy.get('h1').should('have.text', 'Another test information page')
     cy.get('.image').should('have.attr', 'alt', 'Grand Central Terminal in 1914, a year after opening.')
+  })
+  it('Has no detectable a11y violations on load', () => {
+    cy.intercept(
+      '/api/v2/page_preview/*',
+      { fixture: 'aviary/preview-information-draft.json' },
+    ).as('draftInformationPreview')
+    cy.visit('preview?identifier=abc&token=123')
+    cy.wait('@draftInformationPreview')
+
+    cy.injectAxe()
+    cy.checkA11y(null, cypressConfig)
   })
 })
