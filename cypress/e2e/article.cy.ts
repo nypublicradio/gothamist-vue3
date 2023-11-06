15e36a311634bab79143278375c5c445eab79c2d
walsh9 <walsh9@gmail.com>
2023-11-06 11:48:36 -0500

    add small delay to make test more reliable

--- cypress/e2e/article.cy.ts
+++ cypress/e2e/article.cy.ts
@@ -130,6 +130,7 @@ describe('An article page', () => {
       cy.wait('@oldArticle').then(() => {
         cy.get('h2.regwall-header').should('contain', 'Read this story completely free')
         cy.get('.regwall-form-wrapper input[type=email]').type(emailAddress)
+        cy.wait(200)
         cy.contains('Sign Up').click()
         cy.wait('@emailProxy')
         cy.get('h2.regwall-header').should('contain', 'Thanks for subscribing!')
