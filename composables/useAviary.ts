a080091648191238304d932102b02c5178245e38
Elizabeth Flowers <91388428+encodedrose@users.noreply.github.com>
2024-02-09 12:04:05 -0800

    Set key path.

--- composables/useAviary.ts
+++ composables/useAviary.ts
@@ -7,7 +7,7 @@ export function transformResponseData(data: Record<string, any>): Record<string,
 export default async function useAviary(path: string, options: Record<string, any> = {}) {
   const config = useRuntimeConfig()
 
-  const { data, error } = await useFetch(path, { baseURL: config.API_URL ?? config.public.API_URL, ...options })
+  const { data, error } = await useFetch(path, { baseURL: config.API_URL ?? config.public.API_URL, key: path, ...options })
   const transformedData = transformResponseData(data)
   return { data: transformedData, error }
 }
