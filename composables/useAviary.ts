922e82b8a0ec37a9518c58276f12cd4194e25abd
Elizabeth Flowers <91388428+encodedrose@users.noreply.github.com>
2024-02-09 01:00:24 -0800

    Squash all commits.

--- composables/useAviary.ts
+++ composables/useAviary.ts
@@ -6,7 +6,8 @@ export function transformResponseData(data: Record<string, any>): Record<string,
 
 export default async function useAviary(path: string, options: Record<string, any> = {}) {
   const config = useRuntimeConfig()
-  const { data, error } = await useFetch(path, { baseURL: config.public.API_URL, ...options })
+
+  const { data, error } = await useFetch(path, { baseURL: config.API_URL ?? config.public.API_URL, ...options })
   const transformedData = transformResponseData(data)
   return { data: transformedData, error }
 }
