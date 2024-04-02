e6a545fe763ddc67e44bf087c8232c515279a355
walsh9 <walsh9@gmail.com>
2024-04-02 17:53:33 -0400

    update sentry config

--- plugins/sentry.client.js
+++ plugins/sentry.client.js
@@ -9,7 +9,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     app: [vueApp],
     dsn: config.public.SENTRY_DSN,
     integrations: [
-      Sentry.browserTracingIntegration({ router: vueApp.router }),
+      Sentry.browserTracingIntegration({ router: nuxtApp.$router }),
       Sentry.replayIntegration(),
     ],
     tracesSampleRate: config.public.SENTRY_ENV.toUpperCase() === 'PROD' ? 0.5 : 1.0,
