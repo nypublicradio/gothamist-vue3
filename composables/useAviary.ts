7a57f577229c5f35f959b460ebc0788b36e7d139
walsh9 <walsh9@gmail.com>
2024-04-08 12:35:44 -0400

    don't report every 404 to sentry

--- composables/useAviary.ts
+++ composables/useAviary.ts
@@ -8,7 +8,7 @@ export default async function useAviary(path: string, options: Record<string, an
   const config = useRuntimeConfig()
   const { data, error } = await useFetch(path, { baseURL: config.public.API_URL, ...options })
     .then((response) => {
-      if (process.client && response.error.value) {
+      if (process.client && response.error.value && response.error.values.statusCode !== 404) {
         const { $sentry } = useNuxtApp()
         $sentry.captureException(response.error.value)
       }
