27f3a82e94e100d18444ce2ad0d280e87d6589ba
walsh9 <walsh9@gmail.com>
2024-04-09 15:48:37 -0400

    don't save query params in payload

--- composables/useAviary.ts
+++ composables/useAviary.ts
@@ -6,6 +6,11 @@ export function transformResponseData(data: Record<string, any>): Record<string,
 
 export default async function useAviary(path: string, options: Record<string, any> = {}) {
   const config = useRuntimeConfig()
+
+  // don't save the query string in the payload
+  const { payload } = useNuxtApp()
+  delete payload?.path
+
   const { data, error } = await useFetch(path, { baseURL: config.public.API_URL, ...options })
   const transformedData = transformResponseData(data)
   return { data: transformedData, error }
