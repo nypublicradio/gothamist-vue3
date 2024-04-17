a7b1658e1e1aeaa329fa2d5cc3fa1149c0c7a96c
Elizabeth Flowers <91388428+encodedrose@users.noreply.github.com>
2024-04-17 10:43:58 -0700

    Cache the 301s returned for Spam Results.

--- server/middleware/redirect-spam.ts
+++ server/middleware/redirect-spam.ts
@@ -1,3 +1,5 @@
+import { CacheControlAgeTime } from '~/composables/types/CacheControlAgeTime'
+
 export default fromNodeMiddleware((req, res, next) => {
   const url = decodeURI(req.url)
   const regex = /[\p{Script=Han}\p{Script=Hangul}\p{Script=Thai}]/gu
@@ -5,7 +7,8 @@ export default fromNodeMiddleware((req, res, next) => {
 
   if (found.length >= 5) {
     res.writeHead(301, {
-      Location: '/',
+      'Location': '/',
+      'cache-control': `public, max-age=${CacheControlAgeTime.MONTH}`,
     })
     res.end()
   }
