6c936c95914fde68de95e686a2bcec90adf51cd6
Elizabeth Flowers <91388428+encodedrose@users.noreply.github.com>
2024-04-24 13:54:58 -0700

    Set Expires header as well.

--- plugins/cache-control.server.ts
+++ plugins/cache-control.server.ts
@@ -7,6 +7,7 @@ export default defineNuxtPlugin({
 
       if (cacheControlMaxAge.value > 0 && process.env.NODE_ENV !== 'development')
         event.res.setHeader('Cache-Control', `public, max-age=${cacheControlMaxAge.value}`)
+      event.res.setHeader('Expires', new Date(Date.now() + cacheControlMaxAge.value * 1000).toUTCString())
     },
   },
 })
