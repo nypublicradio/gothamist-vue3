fc2a0697d3fef42374341e05de1e439c6940c185
Elizabeth Flowers <91388428+encodedrose@users.noreply.github.com>
2024-05-17 11:49:01 -0700

    Cleaning up.

--- composables/useAviary.ts
+++ composables/useAviary.ts
@@ -7,7 +7,18 @@ export function transformResponseData(data: Record<string, any>): Record<string,
 export default async function useAviary(path: string, options: Record<string, any> = {}) {
   const config = useRuntimeConfig()
 
+  // don't save the query string in the payload
+  const { payload } = useNuxtApp()
+  delete payload?.path
+
   const { data, error } = await useFetch(path, { baseURL: config.API_URL ?? config.public.API_URL, key: path, ...options })
+    .then((response) => {
+      if (process.client && response.error.value && response.error.value.statusCode !== 404) {
+        const { $sentry } = useNuxtApp()
+        $sentry.captureException(response.error.value)
+      }
+      return response
+    })
   const transformedData = transformResponseData(data)
   return { data: transformedData, error }
 }
