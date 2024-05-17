8753964bf4f20a39bb0ca19c0067beea638b890a
Elizabeth Flowers <91388428+encodedrose@users.noreply.github.com>
2024-05-17 11:46:48 -0700

    Allow setting API_URLs for internal/external traffic.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -95,6 +95,8 @@ export default defineNuxtConfig({
     ],
   },
   runtimeConfig: {
+    // URL used by the server to talk to CMS.
+    API_URL: 'http://localhost:81/api/v2',
     public: {
       SENTRY_DSN: process.env.SENTRY_DSN ?? 'https://531d9ce2b7a14759a35f95f3dd1ee743@o557978.ingest.sentry.io/6537168',
       SENTRY_ENV: process.env.SENTRY_ENV ?? 'development',
