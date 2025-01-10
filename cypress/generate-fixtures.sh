891b675ac7ea055ce085e252f70b080c968a6cb0
walsh9 <walsh9@gmail.com>
2025-01-10 17:28:50 -0500

    deduplicate articles in the latest news river

--- cypress/generate-fixtures.sh
+++ cypress/generate-fixtures.sh
@@ -10,9 +10,9 @@ mkdir fixtures/publisher
 # curl "https://cms.prod.nypr.digital/api/v2/navigation/1/" > "fixtures/aviary/navigation.json"
 
 # Home Page
-curl "https://cms.prod.nypr.digital/api/v2/pages/?type=news.ArticlePage&fields=ancestry,description,lead_asset,legacy_id,listing_image,publication_date,show_as_feature,sponsored_content,tags,updated_date,url,uuid,listing_title,listing_summary,related_authors&order=-publication_date&show_on_index_listing=true&limit=6" > "fixtures/aviary/latest.json"
+curl "https://cms.prod.nypr.digital/api/v2/pages/?type=news.ArticlePage&fields=ancestry,description,lead_asset,legacy_id,listing_image,publication_date,show_as_feature,sponsored_content,tags,updated_date,url,uuid,listing_title,listing_summary,related_authors&order=-publication_date&show_on_index_listing=true&limit=16" > "fixtures/aviary/latest.json"
 curl -L "https://cms.prod.nypr.digital/api/v2/pages/find?html_path=/" > "fixtures/aviary/index.json"
-curl "https://cms.prod.nypr.digital/api/v2/pages/?type=news.ArticlePage&fields=ancestry,description,lead_asset,legacy_id,listing_image,publication_date,show_as_feature,sponsored_content,tags,updated_date,url,uuid,listing_title,listing_summary,related_authors&order=-publication_date&show_on_index_listing=true&limit=6&offset=6" > "fixtures/aviary/index-more.json"
+curl "https://cms.prod.nypr.digital/api/v2/pages/?type=news.ArticlePage&fields=ancestry,description,lead_asset,legacy_id,listing_image,publication_date,show_as_feature,sponsored_content,tags,updated_date,url,uuid,listing_title,listing_summary,related_authors&order=-publication_date&show_on_index_listing=true&limit=10&offset=16" > "fixtures/aviary/index-more.json"
 
 # What's On
 curl "https://api.prod.nypr.digital/api/v4/whats_on/?filter\[slug\]=wnyc-fm939&include=current-airing.image,current-show.show.image,current-episode.segments" > "fixtures/publisher/whats_on.json"
