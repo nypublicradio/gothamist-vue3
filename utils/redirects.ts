cb14c3a1add0997e49d939b8efb7e6fb0f137811
Kim LaRocca <kim@kimlarocca.com>
2026-06-15 10:44:02 -0400

    update redirects to handle internal routing too

---
+++ utils/redirects.ts
@@ -0,0 +1,7 @@
+export const redirects = [
+  { from: '/tags/wethecommuters', to: '/tags/we-the-commuters' },
+  { from: '/tags/agata%26valentia', to: '/tags/agata-valentia' },
+  { from: '/tags/agata&valentia', to: '/tags/agata-valentia' },
+  { from: '/tags/Sets%20&%20the%20City%20profile', to: '/tags/sets-and-the-city-profile' },
+  { from: '/news/testing-scrollytelling-article', to: '/workaround-costing-new-york-hundreds-affordable-homes-interactive' },
+]
